e48a277f5d6dc8478d6d4bdcaf0907b282c7caf1
nawanjan <agndrupasinghe@gmail.com>
2026-01-24 03:16:58 +0530

    Fix frontend serving and contact API

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function showMessage() {
-    document.getElementById("message").innerText =
-        "Thank you for exploring SkillBridge!";
+function sendMessage() {
+  const name = document.getElementById("name").value;
+  const message = document.getElementById("message").value;
+
+  if (!name || !message) {
+    document.getElementById("response").innerText =
+      "Please fill all fields";
+    return;
+  }
+
+  fetch("/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name, message })
+  })
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("response").innerText = data.reply;
+      document.getElementById("name").value = "";
+      document.getElementById("message").value = "";
+    })
+    .catch(() => {
+      document.getElementById("response").innerText =
+        "Error sending message";
+    });
 }
